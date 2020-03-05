632dc214d186f91a1a6635324e47ac791962ef2f
rsalinas6721 <47551022+rsalinas6721@users.noreply.github.com>
2020-03-05 00:50:27 -0500

    Add files via upload
    File Transfer Implementation Without sendfile() Method

---
+++ ftclient.py
@@ -0,0 +1,87 @@
+import socket
+import sys
+
+def main():
+	serverLocation = sys.argv[1]
+	controlPort = int(sys.argv[2])
+	command = sys.argv[3]
+
+	if (command == "-l"):
+		dataPort = int(sys.argv[4])
+	elif (command == "-g"):
+		file = sys.argv[4]
+		app = command
+		command = command + file
+		dataPort = int(sys.argv[5])
+
+	serverAddress = 'flip1.engr.oregonstate.edu'
+	establishedConnectionFD = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	establishedConnectionFD.connect((serverAddress, controlPort))
+	if establishedConnectionFD:
+		print("Connected to Server")
+
+	establishedConnectionFD.sendall(command.encode('utf-8'))
+	ack = b''
+	while len(ack) < 5:
+		ack += establishedConnectionFD.recv(5)
+	ack = ack.decode('utf-8')
+	if(len(ack) != 5):
+		print("An Error Occured!")
+	establishedConnectionFD.sendall(str(dataPort).encode('utf-8'))
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.bind(('flip1.engr.oregonstate.edu', dataPort))
+	sock.listen(1)
+	print("Listening on Data Port ", dataPort)
+	connection, clientAddress = sock.accept()
+	print("Established Connection With ", clientAddress)
+
+	if (command == "-l"):
+		print("Receiving Directory Structure")
+		response = b''
+		while len(response) < 1024:
+			response += connection.recv(1024)
+		response = response.decode('utf-8')
+		if(len(response) != 1024):
+			print("An Error Occured!")
+		response = response.split(" . ")
+		response.pop()
+		for item in response:
+			print(item)
+
+	elif(app == "-g"):
+		print("Receiving File")
+		response = bytearray()
+		packet = 1
+		list = []
+		while True:
+#			print("Packet: ", packet)
+			fragment = connection.recv(1024)
+			fragment = bytearray(fragment)
+			counter = 0
+#			print("\n\nBEFORE:")
+#			print(fragment)
+			for item in fragment:
+				if item == 0:
+					fragment.remove(item)
+					list.append(counter)
+				counter = counter + 1
+			if not fragment:
+#				print("Complete")
+				break
+#			print(" \n\nAFTER:")
+#			print(fragment)
+			response.extend(fragment)
+			packet = packet + 1
+#		print("Complete")
+		for item in response:
+			if item == 0:
+				response.remove(item)
+		response = response.decode('utf-8')
+		f = open(file, "w")
+		f.write(response)
+		f.close()
+		print("File Transfer Complete")
+	connection.close()
+	establishedConnectionFD.close()
+
+main()
---
+++ ftserver.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <dirent.h>
+
+int main(int argc, char *argv[]){
+	
+	int listenSocketFD, establishedConnectionFD, controlPort, dataPort, charsWritten, sock;
+	socklen_t sizeOfClientInfo;
+
+// SET UP CONTROL SOCKET -------------------------------------------------------
+	struct sockaddr_in serverAddressOne, serverAddressTwo, clientAddressOne;
+	struct hostent* serverHostInfo;
+	if (argc < 2) {
+		fprintf(stderr,"Please Run %s Followed By Port Number\n", argv[0]); exit(1);
+	}
+	memset((char *)&serverAddressOne, '\0', sizeof(serverAddressOne));
+	controlPort = atoi(argv[1]);
+	serverAddressOne.sin_family = AF_INET;
+	serverAddressOne.sin_port = htons(controlPort);
+	serverAddressOne.sin_addr.s_addr = INADDR_ANY;
+	listenSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocketFD < 0){
+		printf("%s \n", "An Error Occured");
+		exit(1);
+	}
+	if (bind(listenSocketFD, (struct sockaddr *)&serverAddressOne, sizeof(serverAddressOne)) < 0){
+		printf("%s \n", "An Error Occured");
+		exit(1);
+	}
+
+while (1){
+	listen(listenSocketFD, 10);
+	char one = '-';
+	char two = 'g';
+	sizeOfClientInfo = sizeof(clientAddressOne);
+	establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddressOne, &sizeOfClientInfo);
+	if (establishedConnectionFD < 0){
+		printf("%s \n", "An Error Occured");
+		exit(1);
+	}
+	printf("%s \n", "Connection Established");
+
+// Establish Connection Take Command -------------------------------------------
+//	bzero(mess, BUFFSIZE);
+	int valread;
+	char command[25];
+	char dataPortStr[10];
+	char ack[] = "ACK";
+	memset(command, '\0', 20);
+	memset(dataPortStr, '\0', 10);
+	read(establishedConnectionFD, command, 25);
+	write(establishedConnectionFD, ack, 5);
+	memset(dataPortStr, '\0', 10);
+	read(establishedConnectionFD, dataPortStr, 10);
+	int portNum;
+	portNum = atoi(dataPortStr);
+
+// SET UP DATA SOCKET ----------------------------------------------------------
+	printf("Establishing Data Connection on %d \n", portNum);
+	serverAddressTwo.sin_family = AF_INET;
+	serverAddressTwo.sin_port = htons(portNum);
+	serverHostInfo = gethostbyname("flip1.engr.oregonstate.edu");
+	memcpy((char*)&serverAddressTwo.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	int yes = 1;
+	setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+	sleep(1);
+	if(connect(sock, (struct sockaddr*)&serverAddressTwo, sizeof(serverAddressTwo)) < 0){
+		printf("%s\n", "An Error Occured");
+		exit(1);
+	}
+
+// Execute Command -l -------------
+	char dirComm[] = "-l";
+	if (strcmp(command, dirComm) == 0){
+		char directory[1024];
+		char spacing[] = " . ";
+		memset(directory, '\0', 1024);
+		DIR *d;
+		struct dirent *dir;
+		d = opendir(".");
+		if (d){
+			while ((dir = readdir(d)) != NULL){
+				if(strcmp(dir->d_name, ".") == 0){
+					continue;
+				}
+				if(strcmp(dir->d_name, "..") == 0){
+					continue;
+				}
+				strcat(directory, dir->d_name);
+				strcat(directory, spacing);
+			}
+			closedir(d);
+		}
+		write(sock, directory, 1024);
+		printf("%s\n", directory);
+	}
+
+// Execute Command -g -------------
+	else if ((command[0] == one) && (command[1] == two)){
+		printf("%s\n", "Sending File");
+		FILE *fp;
+		memmove(command, command+2, strlen(command));
+		char buffer[1024];
+		memset(buffer, 0, sizeof(buffer));
+		int fd = open(command, O_RDONLY);
+		while (1){
+			int fileCount = read(fd, buffer, sizeof(buffer)-1);
+			if (fileCount == 0){
+				break;
+			}
+			void *buff = buffer;
+			while(fileCount > 0) {
+				int packetsSent = send(sock, buff, sizeof(buffer), 0);
+				fileCount = fileCount - packetsSent;
+				buff = buff + packetsSent;
+			}
+			memset(buffer, 0, sizeof(buffer));
+		}
+	}
+
+	close(sock);
+	close(establishedConnectionFD);
+	}
+	close(listenSocketFD);
+	return 0;
+}
